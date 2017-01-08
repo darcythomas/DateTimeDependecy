56ab327b0d24cc7a6bffe67e1c078a035b40a348
Darcy <github@darcythomas.com>
2017-01-09 11:40:50 +1300

    added dateTime Search stub

---
+++ DependableDateTime/DateTimeHunter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependableDateTime
+{
+    class DateTimeHunter
+    {
+        //public IEnumerable<MethodInfo> FindAllUsages()
+        //{
+        //   // var methodBase = typeof(DateTime).GetRuntimeProperty("Now");
+
+        //    var token = typeof(DateTime).GetRuntimeProperty("TargetProp").GetMethod;
+
+        //    // const BindingFlags findAll = BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.NonPublic |
+        //    //                              BindingFlags.Instance | BindingFlags.Static;
+
+        //    AppDomain.CurrentDomain.GetAssemblies();
+        //    var references =
+        //        typeof(DateTimeHunter).a.ManifestModule.GetTypes()
+        //            .SelectMany(x => x.GetMethods(Int32.MaxValue).Cast<MethodBase>().Union(x.GetConstructors(Int32.MaxValue)))
+        //            .ToDictionary(y => y, y => y.GetMethodUsageOffsets(token).ToArray())
+        //            .Where(z => z.Value.Length > 0).ToList();
+
+        //}
+    }
+}
