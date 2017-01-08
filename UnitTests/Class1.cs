956903d179fa474d45a8494468e38e48ef0804db
Darcy <github@darcythomas.com>
2017-01-09 10:29:50 +1300

    basic unit test proj

---
+++ UnitTests/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace UnitTests
+{
+    public class Class1
+    {
+        [Fact]
+        public void TestBasics()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
